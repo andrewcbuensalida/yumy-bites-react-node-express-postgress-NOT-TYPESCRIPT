2b59e2fe8107d89fca8f3ca245e0fe1353601657
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-03-02 20:58:31 -0800

    another

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.use(express.static(path.join(__dirname, "client/build")));
 	app.use(express.static("public"));
 }
 
@@ -64,7 +64,7 @@ app.post("/api/v1/customer", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "..client/build", "index.html"));
+	res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 3500;
